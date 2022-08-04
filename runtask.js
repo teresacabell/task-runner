e7826d72fa6f4264b9f9ea2f4993c0172d454919
teresacabell <teresacabll@gmail.com>
2022-08-04 17:26:09 -0500

    worked on priority

--- runtask.js
+++ runtask.js
@@ -1,51 +1,58 @@
-function runtask(s,d,p) {
-    // note: i is place in array, 0 <= i <= (n-1)
-    // find how many tasks are in the input (n) by checking (s)
-    n = len(s);
+function runtask(s, d, p) {
+  // note: i is place in array, 0 <= i <= (n-1)
+  // find how many tasks are in the input (n) by checking (s)
+  n = len(s);
 
-    // create array for solution
-    task_order = []
+  // create array for solution
+  task_order = [];
 
-    // define i
-    // add the tasks to the array
+  for (i = 0; i < n; i++) {
+    task_order.push[i];
+  }
 
-    // define j = n-i-1
+  // define time
+  time = 0;
 
-    // define time
-        time = 0
-   
-    // compare priority (p)
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < i; j++) {
+      // compare priority (p)
+      // if priority for one is higher than all others
+      if (p[j] < p[j + 1]) {
+        // append to solution array
+        task_order.push[i] = task_order;
 
-        // if priority for one is higher than all others, append to solution array
+        // calculate current time
 
-            // calculate current time 
+        // exit loop to check priority for remaining tasks
+      } else {
+      }
 
-                // exit loop to check priority for remaining tasks
+      // if same priority, compare start time (s)
 
-        // if same priority, compare start time (s)
+      // if one start time for one is higher than all others, append to solution array
 
-            // if one start time for one is higher than all others, append to solution array
-                
-                // calculate current time 
+      // calculate current time
 
-                    // exit loop to check priority for remaining tasks
+      // exit loop to check priority for remaining tasks
 
-            // if same start time, compare duration (d)
+      // if same start time, compare duration (d)
 
-                // if one duration is higher than all others, append to solution array
+      // if one duration is higher than all others, append to solution array
 
-                    // calculate current time 
-                    
-                        // exit loop to check priority for remaining tasks
+      // calculate current time
 
-                // if same duration, compare place in array (i)
+      // exit loop to check priority for remaining tasks
 
-                    // if one i is higher than others, append to solution array
+      // if same duration, compare place in array (i)
 
-                        // calculate current time
+      // if one i is higher than others, append to solution array
 
-                            // exit loop to check priority for remaining tasks
+      // calculate current time
 
-    // give solution array
-    return task_order;
+      // exit loop to check priority for remaining tasks
+    }
+  }
+
+  // give solution array
+  return task_order;
 }
